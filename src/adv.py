eee3037c459043efeb09052889d8bf5af3ae269e
dgansen <dtgansen@gmail.com>
2020-08-07 10:37:06 -0500

    almost done

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -19,6 +20,7 @@ to north. The smell of gold permeates the air."""),
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
+    'blank': Room('Initializer Room', "It's amazing how little there is in this room. A marvel to behold.")
 }
 
 
@@ -33,12 +35,33 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+# # Initializer
+# room['blank'].n_to = room['blank']
+# room['blank'].s_to = room['blank']
+# room['blank'].e_to = room['blank']
+# room['blank'].w_to = room['blank']
+
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player(room['outside'])
+dir_list = ['n','s','e','w']
+def show_loc():
+    print(f'Location: {player.current_room.name}\n{player.current_room.description}')
+def move(cur_room, direction):
+    # Returns new room object
+    move_dict = {'n':cur_room.n_to,    
+                 's':cur_room.s_to,
+                 'e':cur_room.e_to,
+                 'w':cur_room.w_to}
+    print(cur_room.description)
+    print(cur_room.n_to.description)
+    print(move_dict[direction])
+    return move_dict[direction]
 
+#
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +72,37 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+print('Enter "b" to begin the game:')
+while True:
+    command = input('>').split(',')
+    # check if given command is more than 1?
+
+    if command[0] == 'b':
+        #Begin the game
+        print("The air hangs acrid and heavy, bitterly smoky like ground, bare metal.",
+        "Amidst rubble and ruin, you approach with fatigued but determined steps.",
+        "Your boots are wet, your mouth is dry.",
+        "No normal person would undergo the challenge you have consigned to perform.",
+        "But despite it all, here you are, standing before the Cave, dealer of death and destinies, the Docker container of the treasure and glory you seek.",
+        "Enter `h` for help, you freak.",
+        sep='\n')
+
+    # quit command
+    elif command[0]=='q':
+        break
+    # help command
+    elif command[0] == 'h':
+        print('''Type your commands separated by a comma:\n
+         - "n" to move north\n
+         - "s" to move south\n
+         - "w" to move west\n
+         - "e" to move east\n
+         - "h" for help\n
+         - "q" to quit the game''')
+    elif command[0] in dir_list:
+        player.current_room = move(player.current_room, command[0])
+    
+    # "I don't understand" else block
+    else:
+        print("I'm afraid that's not a command")
+    show_loc()
